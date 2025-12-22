600d3351336772c6375013ae46c1391cccf8ee2b
普罗米修斯 <118711195+pengchao2022@users.noreply.github.com>
2025-12-22 17:30:32 +0800

    Create cpu.js

---
+++ nodejs打印macCPU/cpu.js
@@ -0,0 +1,60 @@
+
+const os = require('os')
+console.log(os.cpus())
+
+
+# 运行结果
+
+pengchaoma@pengchaos-MacBook-Pro node_tutorial % node ./index.js
+[
+  {
+    model: 'Apple M5',
+    speed: 2400,
+    times: { user: 96954140, nice: 0, sys: 64971780, idle: 478851050, irq: 0 }
+  },
+  {
+    model: 'Apple M5',
+    speed: 2400,
+    times: { user: 79538200, nice: 0, sys: 48759480, idle: 513572660, irq: 0 }
+  },
+  {
+    model: 'Apple M5',
+    speed: 2400,
+    times: { user: 58864300, nice: 0, sys: 31875750, idle: 553045340, irq: 0 }
+  },
+  {
+    model: 'Apple M5',
+    speed: 2400,
+    times: { user: 38523290, nice: 0, sys: 19593570, idle: 587428710, irq: 0 }
+  },
+  {
+    model: 'Apple M5',
+    speed: 2400,
+    times: { user: 26513440, nice: 0, sys: 13163620, idle: 606920240, irq: 0 }
+  },
+  {
+    model: 'Apple M5',
+    speed: 2400,
+    times: { user: 19420450, nice: 0, sys: 8977930, idle: 619026940, irq: 0 }
+  },
+  {
+    model: 'Apple M5',
+    speed: 2400,
+    times: { user: 14617030, nice: 0, sys: 3530170, idle: 630014670, irq: 0 }
+  },
+  {
+    model: 'Apple M5',
+    speed: 2400,
+    times: { user: 12788480, nice: 0, sys: 2820380, idle: 632608920, irq: 0 }
+  },
+  {
+    model: 'Apple M5',
+    speed: 2400,
+    times: { user: 11027020, nice: 0, sys: 2294800, idle: 634957320, irq: 0 }
+  },
+  {
+    model: 'Apple M5',
+    speed: 2400,
+    times: { user: 9952130, nice: 0, sys: 1896160, idle: 636508030, irq: 0 }
+  }
+]
